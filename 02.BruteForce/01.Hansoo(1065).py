c699c1d5271428d64483f6adf8fe3459bd6dc95b
최서연 <gkdltjdus@gmail.com>
2021-01-27 16:59:28 +0900

    그리디 알고리즘을 활용한 백준 알고리즘 문제 풀이, 2437번 & 1783번(미해결)

---
+++ 02.BruteForce/01.Hansoo(1065).py
@@ -0,0 +1,15 @@
+n = int(input())
+result = 0
+
+if n < 100:
+    result += n
+elif n >= 100:
+    result += 99
+    for n in range(100, n+1):
+        one = n // 100
+        two = (n % 100) // 10
+        thr = (n % 100) % 10
+        if (one - two) == (two - thr):
+            result += 1
+
+print(result)
---
+++ 02.BruteForce/02.BlackJack(2798).py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+result = cards[0] + cards[1] + cards[2]
+
+for x in range(n-2):
+    for y in range(x+1, n-1):
+        for z in range(y+1, n):
+            num = cards[x] + cards[y] + cards[z]
+            if abs(m - num) < abs(m - result) and num <= m:
+                result = num
+print(result)
