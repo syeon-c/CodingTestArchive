4ce02a23716ae4a97ea0aca2a4186bac46c7c14a
최서연 <gkdltjdus@gmail.com>
2021-02-03 17:06:40 +0900

    Try to solve #14501 & #1018

---
+++ 02.BruteForce/06.Resignation(14501).py
@@ -0,0 +1,14 @@
+n = int(input())
+schedule = [list(map(int, input().split())) for _ in range(n)]
+
+def process(x, res):
+    cost = 0
+    # x가 주어진 기간보다 클 경우(퇴사 후)
+    if x > n: return 0
+    # x가 주어진 기간만큼일 경우 결과 반환
+    if x == n: return res
+    # 지금까지의 수입과, x의 수익과, 그 다음 요소인 x+1 중 수입이 최대인 값을 저
+    cost = max(cost, process(x + schedule[x][0], res + schedule[x][1]), process(x + 1, res))
+    return cost
+
+print(process(0, 0))
---
+++ 02.BruteForce/07.RepaintChessboard(1018).py
@@ -0,0 +1,5 @@
+n, m = map(int, input().split())
+chess = [list(map(str, list(input()))) for _ in range(n)]
+result = 0
+
+print(result)
