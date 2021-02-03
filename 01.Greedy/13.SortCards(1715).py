0851dca46fa3cc4147231c20d4e861ed3f0b15cc
최서연 <gkdltjdus@gmail.com>
2021-02-03 14:37:51 +0900

    Create Python file , BruteForce Problems #14501 & #1018

--- 01.Greedy/13.SortCards(1715).py
+++ 01.Greedy/13.SortCards(1715).py
@@ -1,12 +1,12 @@
-import sys
-
 n = int(input())
 cards = [int(input()) for _ in range(n)]
 
 cards.sort()
-
-result = cards[0] + cards[1]
-for i in range(2, len(cards)):
-    result += result + cards[i]
+if n == 1:
+    result = 0
+else:
+    result = cards[0] + cards[1]
+    for i in range(2, len(cards)):
+        result += result + cards[i]
 
 print(result)
