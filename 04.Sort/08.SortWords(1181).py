4dcb1d72647ebcd5f927932ca370d25cb8a7338e
최서연 <gkdltjdus@gmail.com>
2021-02-22 14:00:18 +0900

    #Sorting #1181 & #10814

--- 04.Sort/08.SortWords(1181).py
+++ 04.Sort/08.SortWords(1181).py
@@ -1,3 +1,16 @@
 import sys
 
 n = int(sys.stdin.readline())
+wordsList = []
+
+for _ in range(n):
+    word = str(sys.stdin.readline())
+    wordLen = len(word)
+    wordsList.append((word, wordLen))
+
+wordsList = list(set(wordsList))
+
+wordsList.sort(key=lambda word: (word[1], word[0]))
+
+for word in wordsList:
+    print(word[0], end="")
--- 04.Sort/09.SortAges(10814).py
+++ 04.Sort/09.SortAges(10814).py
@@ -1,2 +1,13 @@
 import sys
 n = int(sys.stdin.readline())
+memberList = []
+
+for _ in range(n):
+    age, name = map(str, sys.stdin.readline().split())
+    age = int(age)
+    memberList.append((age, name))
+
+memberList.sort(key=lambda member: (member[0]))
+
+for member in memberList:
+    print(member[0], member[1])
