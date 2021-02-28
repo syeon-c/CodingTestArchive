48870281159f574cf5e83e6e2bb35ed4af00cb12
최서연 <gkdltjdus@gmail.com>
2021-02-28 18:32:46 +0900

    백트래킹 깊이우선탐색 스택 구현 & N과 M(2)

--- 05.BackTracking/02.NandM2(15650).py
+++ 05.BackTracking/02.NandM2(15650).py
@@ -1,3 +1,25 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
 
+n, m = map(int, sys.stdin.readline().split())
+nums = [1 + i for i in range(n)]
+visited = [False] * n
+result = []
+
+
+def DFS(x, idx):
+    if x == m:
+        print(*result)
+        return
+
+    for i in range(idx, n):
+        if not visited[i]:
+            visited[i] = True
+            result.append(nums[i])
+
+            DFS(x + 1, idx + 1)
+
+            visited[i] = False
+            result.pop()
+
+
+DFS(0, 0)
