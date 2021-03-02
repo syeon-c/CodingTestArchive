4221798ebe688324b480efa099e831e06fd12eb6
syeon-c <gkdltjdus@gmail.com>
2021-03-02 15:52:09 +0900

    BackTracking #15650 & #15652 Solved

--- 05.BackTracking/02.NandM2(15650).py
+++ 05.BackTracking/02.NandM2(15650).py
@@ -10,15 +10,15 @@ def DFS(x):
         print(*result)
         return
 
-    for i in range(x, n):
+    for i in range(n):
         if not visited[i]:
             visited[i] = True
             result.append(i+1)
-
             DFS(x + 1)
-
-            visited[i] = False
             result.pop()
+
+            for j in range(i+1, n):
+                visited[j] = False
 
 
 DFS(0)
--- 05.BackTracking/04.NandM4(15652).py
+++ 05.BackTracking/04.NandM4(15652).py
@@ -5,20 +5,21 @@ visited = [False] * n
 result = []
 
 
-def DFS(x, y):
+def DFS(x):
     if x == m:
         print(*result)
         return
 
-    for i in range(y, n):
+    for i in range(n):
         if not visited[i]:
+            result.append(i + 1)
+
+            DFS(x + 1)
             visited[i] = True
-            result.append(i+1)
 
-            DFS(x + 1, y + 1)
-
-            visited[i] = False
             result.pop()
+            for j in range(i + 1, n):
+                visited[j] = False
 
 
-DFS(0, 0)
+DFS(0)
