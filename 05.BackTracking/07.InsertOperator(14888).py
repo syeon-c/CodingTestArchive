0ea3c727370b3f52c203193d894af7c65d288fcf
최서연 <gkdltjdus@gmail.com>
2021-03-13 17:31:33 +0900

    BOJ/BackTracking/#14888 Trying...

--- 05.BackTracking/07.InsertOperator(14888).py
+++ 05.BackTracking/07.InsertOperator(14888).py
@@ -1,15 +1,43 @@
-from collections import deque
 import sys
 
-def func(cnt):
-    global max_res, min_res
+
+def operation(i, a, b):
+    if i == 0:
+        return a + b
+    elif i == 1:
+        return a - b
+    elif i == 2:
+        return a * b
+    elif i == 3:
+        if a < 0:
+            ans = (abs(a) // b)
+            return -ans
+        else:
+            return a // b
+
+
+def dfs(cnt, val):
+    global min_res, max_res
+
+    if cnt == n:
+        max_res = max(val, max_res)
+        min_res = min(val, min_res)
+        return
+
+    for i in range(4):
+        if op[i] > 0:
+            val = operation(i, nums[cnt], nums[cnt + 1])
+            op[i] -= 1
+            dfs(cnt + 1, val)
 
 
 n = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
 op = list(map(int, sys.stdin.readline().split()))
+max_res = float('Inf')
+min_res = float('-Inf')
 
-func(0)
+dfs(0, nums[0])
 
 print(max_res)
 print(min_res)
