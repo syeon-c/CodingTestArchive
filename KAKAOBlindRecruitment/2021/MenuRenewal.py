172841fb84000e746bed5c0ac503914cb9bde35a
최서연 <gkdltjdus@gmail.com>
2021-03-18 21:51:20 +0900

    Programmers/KAKAO/2021/MenuRenewal Solved!

---
+++ KAKAOBlindRecruitment/2021/MenuRenewal.py
@@ -0,0 +1,24 @@
+from itertools import combinations
+from collections import Counter
+
+
+def solution(orders, course):
+    answer = []
+    for i in course:
+        temp = []
+        for order in orders:
+            combi = combinations(sorted(order), i)
+            temp += combi
+        count = Counter(temp)
+        if len(count) != 0 and max(count.values()) > 1:
+            for j in count:
+                if count[j] == max(count.values()):
+                    answer += [''.join(j)]
+
+    return sorted(answer)
+
+
+orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+course = [2, 3, 5]
+
+print(solution(orders, course))
