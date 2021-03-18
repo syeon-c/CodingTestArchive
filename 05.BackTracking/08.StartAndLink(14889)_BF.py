55d52c5c284044d4c183501cf6c50b49cc64fb65
최서연 <gkdltjdus@gmail.com>
2021-03-18 20:20:29 +0900

    BOJ/BackTracking/#14889 Solved

--- 05.BackTracking/08.StartAndLink(14889)_BF.py
+++ 05.BackTracking/08.StartAndLink(14889)_BF.py
@@ -5,9 +5,10 @@ from itertools import combinations
 input = sys.stdin.readline
 
 
+# 팀의 능력치 계산
 def getAbility(team_list):
     sum = 0
-    for i in range(len(team_list) - 1):
+    for i in range(len(team_list)):
         for j in range(i + 1, len(team_list)):
             sum += matrix[team_list[i]][team_list[j]] + matrix[team_list[j]][team_list[i]]
     return sum
@@ -16,6 +17,7 @@ def getAbility(team_list):
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 members = [i for i in range(n)]
+# 팀 분배
 team = deque(combinations(members, n // 2))
 min_res = sys.maxsize
 
@@ -25,10 +27,8 @@ link = deque()
 for _ in range(len(team) // 2):
     start.append(team.popleft())
     link.append(team.pop())
-    print(start)
-    print(link)
 
-for i in range(n // 2):
+for i in range(len(start)):
     res = abs(getAbility(start[i]) - getAbility(link[i]))
     if min_res > res:
         min_res = res
