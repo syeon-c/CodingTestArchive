8b361041f4a40d00fdc6236e519f1aa87bfc098e
최서연 <gkdltjdus@gmail.com>
2021-03-30 15:04:38 +0900

    파이썬기초/DFS/이진트리순회

---
+++ 06.DFS_BFS/Tutorial/BinTreeSearch.py
@@ -0,0 +1,32 @@
+def dfs_search_left(x):
+    if x > 7:
+        return
+    else:
+        print(x, end=' ')
+        dfs_search_left(x * 2)
+        dfs_search_left(x * 2 + 1)
+
+
+def dfs_search_parent(x):
+    if x > 7:
+        return
+    else:
+        dfs_search_parent(x * 2)
+        print(x, end=' ')
+        dfs_search_parent(x * 2 + 1)
+
+
+
+def dfs_search_right(x):
+    if x > 7:
+        return
+    else:
+        dfs_search_right(x * 2)
+        dfs_search_right(x * 2 + 1)
+        print(x, end=' ')
+
+dfs_search_left(1)
+print()
+dfs_search_parent(1)
+print()
+dfs_search_right(1)
