84f08100a02dd6a10b030337c267e13ea0f593ef
최서연 <gkdltjdus@gmail.com>
2021-04-04 23:46:24 +0900

    깊이 우선 탐색 기초/휴가

--- 06.DFS_BFS/Vacation.py
+++ 06.DFS_BFS/Vacation.py
@@ -1,1 +1,25 @@
+def dfs(i, sum):
+    global result
+    if i == n + 1:
+        if sum < result:
+            result = sum
+        else:
+            #
+            if i + T[i] <= n + 1:
+                dfs(i + T[i], sum + P[i])
+            dfs(i + 1, sum)
+
+
 n = int(input())
+T = []
+P = []
+for i in range(n):
+    t, p = map(int, input().split())
+    T.append(t)
+    P.append(p)
+result = -1
+# 1-n 이기 때문에 인덱스 0 채워줌
+T.insert(0, 0)
+P.insert(0, 0)
+dfs(1, 0)
+print(result)
