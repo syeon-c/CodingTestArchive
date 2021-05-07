57f4516341b72d90f192e96686dbaac187adb248
최서연 <gkdltjdus@gmail.com>
2021-05-08 02:19:50 +0900

    프로그래머스/2020 카카오 인턴십/보석쇼핑

---
+++ KAKAOBlindRecruitment/2020Internship/JewerlyShopping.py
@@ -0,0 +1,34 @@
+def solution(gems):
+    answer = []
+    shortestLen = len(gems) + 1
+    start, end = 0, 0
+    gemNum = len(set(gems))
+    gems_dict = {}
+
+    while end < len(gems):
+        if gems[end] not in gems_dict:
+            gems_dict[gems[end]] = 1
+        else:
+            gems_dict[gems[end]] += 1
+
+        end += 1
+
+        if len(gems_dict) == gemNum:
+            while start < end:
+                if gems_dict[gems[start]] > 1:
+                    gems_dict[gems[start]] -= 1
+                    start += 1
+                    print(start)
+                elif shortestLen > (end - start):
+                    shortestLen = (end - start)
+                    answer = [start + 1, end]
+                    print(answer)
+                    break
+                else:
+                    break
+
+    return answer
+
+
+gems = ["AA", "AB", "AC", "AA", "AC"]
+print(solution(gems))
