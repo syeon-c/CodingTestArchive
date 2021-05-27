36413f6e3b0591164f7307628e15e665f4d8dac6
최서연 <gkdltjdus@gmail.com>
2021-05-27 23:07:34 +0900

    Programmers/Hash/Camouflage Solved

--- Hash/Camouflage.py
+++ Hash/Camouflage.py
@@ -1,5 +1,15 @@
 def solution(clothes):
-    answer = 0
     dict = {}
+    for cloth in clothes:
+        if cloth[1] in dict.keys():
+            dict[cloth[1]].append(cloth[0])
+        else:
+            dict[cloth[1]] = [cloth[0]]
+    answer = 1
+    for val in dict.values():
+        answer *= len(val) + 1
+    return answer - 1
 
-    return answer
+
+clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+print(solution(clothes))
