c4b0a1a07e36ae6c5a61bda1ec2f536106ef86af
최서연 <gkdltjdus@gmail.com>
2021-06-13 15:06:22 +0900

    BOJ/삼성 SW역량 테스트 기출 문제/퇴사

---
+++ 삼성SW역량 테스트 기출 문제/퇴사.py
@@ -0,0 +1,24 @@
+def dfs(i, sum, n, T, P):
+    global result
+    if i > n:
+        if result < sum:
+            result = sum
+    else:
+        if i + T[i] <= n + 1:
+            dfs(i + T[i], sum + P[i], n, T, P)
+        dfs(i+1, sum, n, T, P)
+
+
+n = int(input())
+T = []
+P = []
+for i in range(n):
+    t, p = map(int, input().split())
+    T.append(t)
+    P.append(p)
+
+result = -1
+T.insert(0, 0)
+P.insert(0, 0)
+dfs(1, 0, n, T, P)
+print(result)
