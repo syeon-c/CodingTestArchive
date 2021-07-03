b487948a122d221ddf8d6ca573846377fccc6a95
최서연 <gkdltjdus@gmail.com>
2021-07-03 12:47:13 +0900

    BOJ/DFSBFS/Maze Solved

--- 06.DFS_BFS/Maze.py
+++ 06.DFS_BFS/Maze.py
@@ -3,7 +3,7 @@ dy = [0, 1, 0, -1]
 
 n, m = map(int, input().split())
 
-maze = list((input().rstrip()) for _ in range(n))
+maze = [list(map(int, list(input()))) for _ in range(n)]
 
 visited = [[0] * m for _ in range(n)]
 
@@ -12,18 +12,13 @@ visited[0][0] = 1
 
 while Q:
     tmp = Q.pop()
-
-    if tmp[0] == (n - 1) and tmp[1] == (m - 1):
-        print(visited[tmp[0]][tmp[1]])
-        break
-
     for i in range(4):
         x = tmp[0] + dx[i]
         y = tmp[1] + dy[i]
 
         if 0 <= x < n and 0 <= y < m:
-            if visited[x][y] == 0 and maze[x][y] == '1':
+            if visited[x][y] == 0 and maze[x][y] == 1:
                 visited[x][y] = visited[tmp[0]][tmp[1]] + 1
                 Q.append((x, y))
 
-print(maze)
+print(visited[n-1][m-1])
